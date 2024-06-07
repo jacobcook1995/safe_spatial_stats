08c5dce464b48d267bcff58a2b24cf96fe4ce885
Jacob Cook <jc2017@ic.ac.uk>
2024-06-07 14:59:43 +0100

    Fleshed out download script, so that it now (mostly) works

--- R/download_script.R
+++ R/download_script.R
@@ -1,25 +1,23 @@
 library(safedata)
 
 # Find safedata directory to download data to and set
-my_safe_directory <- getOption("safedata.dir")
-set_safe_dir(my_safe_directory)
+set_safe_dir("~/Documents/VirtualRainforest/datasets")
 
-# Want to include the microclimate data, so include it's project ID
-microclimate_id <- ???? #  https://zenodo.org/records/7893600
+# Want to include the microclimate data, so include its record ID
+microclimate_id <- 7893600 # https://zenodo.org/records/7893600
 
 # Want to include roughness estimates
-roughness_id <- ???? # https://zenodo.org/records/3697796
+roughness_id <- 3697796 # https://zenodo.org/records/3697796
 
 # And the derived hydrological data
-hydro_id <- ?????? # https://zenodo.org/records/3490687
-
+hydro_id <- 3490687 # https://zenodo.org/records/3490687
 
 # Finally we want to include the SRTM elevation data, there is also ASTER elevation data
 # but we only want to include one elevation dataset and believe SRTM to be more
 # reliable.
-elevation_id <- ???? # https://zenodo.org/records/3490488
+elevation_id <- 3490488 # https://zenodo.org/records/3490488
 
-tif_data_ids <- list(microclimate_id, roughness_id, hydro_id, elevation_id)
+tif_data_ids <- c(microclimate_id, roughness_id, hydro_id, elevation_id)
 
 # We have already identified which datasets we want to download already, so we load them
 # based on record ids. The option to download all files is needed here as we explictly
