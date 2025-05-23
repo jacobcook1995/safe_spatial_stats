1d4599a211e9658be5008bef26be436473caa12a
Jacob Cook <jc2017@ic.ac.uk>
2025-05-23 09:45:03 +0100

    Added files related to code quality (R/python/markdown) checks

---
+++ r_requirements.R
@@ -0,0 +1,11 @@
+# This file is used to record R packages used by the project.
+#
+# We did look at using `renv` to manage this automatically but it added a layer of
+# complexity and unexpected issues and workflow problems and so we have dropped it for
+# now.
+
+# This is required for R support in VSCode
+library(languageserver)
+
+# This is required to use R with Jupyter
+library(IRkernel)
