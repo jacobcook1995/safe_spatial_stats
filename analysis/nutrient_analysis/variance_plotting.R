1c71a754b25abdc5b9aa6f59b0dce6d1dbf0fe03
jacobcook1995 <jc2017@ic.ac.uk>
2026-03-16 14:27:23 +0000

    Wrote script to plot the variance of my soil data

---
+++ analysis/nutrient_analysis/variance_plotting.R
@@ -0,0 +1,181 @@
+# Basic script to look the variance of my soil nutrient data
+library(terra)
+library(sf)
+library(dplyr)
+
+plot_variances <-
+  function(plot_mean, plot_sd, x_axis, plot_name, plot_title, x_unit, y_unit) {
+    #' Plot the variances of a given set of data
+    #'
+    #' This is written as a function because I basically repeat the same steps for
+    #' four different variables.
+    #'
+    #' @param plot_mean Mean values of variable across the plots
+    #' @param plot_sd Standard deviations of the variable across the plots
+    #' @param x_axis Values to plot against on the x-axis
+    #' @param plot_name File name to save plot under
+    #' @param plot_title Title for the plot
+    #' @param x_unit Unit of the plots x-axis
+    #' @param y_unit Unit of the plots y-axis
+    #' @return
+    #' @export
+
+    png(plot_name, width = 1200, height = 900)
+
+    # Increase margin sizes so that labels don't get clipped off
+    par(mar = c(6, 7, 4, 2))
+
+    # Colour code values without standard deviations
+    cols <- ifelse(is.na(plot_sd), "red", "black")
+
+    plot(x_axis, plot_mean,
+      ylim = c(
+        min(plot_mean - plot_sd, na.rm = TRUE),
+        max(plot_mean + plot_sd, na.rm = TRUE)
+      ),
+      pch = 16, xlab = x_unit, ylab = y_unit, col = cols,
+      main = plot_title, cex.lab = 2, cex.main = 2
+    )
+
+    # Only rows where sd is not NA
+    idx <- !is.na(plot_sd)
+
+    legend("topright",
+      legend = c("Bulked sample", "Subsampled"),
+      col = c("red", "black"), pch = 16
+    )
+
+    arrows(
+      x0 = x_axis[idx],
+      y0 = plot_mean[idx] - plot_sd[idx],
+      x1 = x_axis[idx],
+      y1 = plot_mean[idx] + plot_sd[idx],
+      angle = 90,
+      code = 3,
+      length = 0.05
+    )
+
+    # Save plot by closing
+    dev.off()
+  }
+
+# First load in the data (only care about the plot level summaries). Need to
+# make sure to skip header information.
+plot_data <- readxl::read_xlsx(
+  "./output/SAFE_soil_nutrient_data.xlsx",
+  sheet = "PlotData", skip = 4
+)
+
+# Convert standard deviation data to be numeric
+plot_data$sd_total_carbon <- as.numeric(plot_data$sd_total_carbon)
+plot_data$sd_total_nitrogen <- as.numeric(plot_data$sd_total_nitrogen)
+plot_data$sd_total_phosphorus <- as.numeric(plot_data$sd_total_phosphorus)
+plot_data$sd_available_phosphorus <- as.numeric(plot_data$sd_available_phosphorus)
+
+# Read in the safe project gazetteer
+gazetteer <- st_read(file.path("./output/gazetteer.geojson"))
+
+# Reproject to UTM zone 50N
+reprojected_gazetteer <- st_transform(gazetteer, crs = "EPSG:32650")
+
+# Find new values for the centroids and replace the values in the gazetteer
+centroids <- st_centroid(reprojected_gazetteer)
+
+reprojected_gazetteer <- reprojected_gazetteer %>%
+  mutate(
+    centroid_x = st_coordinates(centroids)[, 1],
+    centroid_y = st_coordinates(centroids)[, 2]
+  )
+
+
+# Join the gazetteer data to the soil data based on plot codes
+plot_data <- plot_data %>%
+  left_join(
+    st_drop_geometry(reprojected_gazetteer),
+    by = c("plot_code" = "location")
+  )
+
+# Load in elevation data
+elevation_raster <- rast(file.path("./primary/SRTM_UTM50N_processed.tif"))
+
+# Convert centroids from plot_data into points, and then extract relevant
+# elevation values based on them
+plot_centers <-
+  vect(plot_data, geom = c("centroid_x", "centroid_y"), crs = crs(elevation_raster))
+
+plot_data$elevation <- extract(elevation_raster, plot_centers)[, 2]
+
+# Load in EVI data and add as a column
+evi_raster <- rast(file.path("./output/Sabah_EVI_2024_Q1.tif"))
+plot_data$evi <- extract(evi_raster, plot_centers)[, 2]
+
+
+# Look at variation vs elevation first
+
+# For total carbon
+plot_variances(
+  plot_mean = plot_data$total_carbon, plot_sd = plot_data$sd_total_carbon,
+  x_axis = plot_data$elevation, plot_name = "figures/total_carbon_vs_elevation.png",
+  plot_title = "Variation in total carbon with elevation",
+  x_unit = "Elevation (m)", y_unit = "Total Carbon (%)"
+)
+
+# Total nitrogen
+plot_variances(
+  plot_mean = plot_data$total_nitrogen, plot_sd = plot_data$sd_total_nitrogen,
+  x_axis = plot_data$elevation, plot_name = "figures/total_nitrogen_vs_elevation.png",
+  plot_title = "Variation in total nitrogen with elevation",
+  x_unit = "Elevation (m)", y_unit = "Total Nitrogen (%)"
+)
+
+# Total phosphorus
+plot_variances(
+  plot_mean = plot_data$total_phosphorus, plot_sd = plot_data$sd_total_phosphorus,
+  x_axis = plot_data$elevation, plot_name = "figures/total_phosphorus_vs_elevation.png",
+  plot_title = "Variation in total phosphorus with elevation",
+  x_unit = "Elevation (m)", y_unit = "Total Phosphorus (mg/kg)"
+)
+
+# Available phosphorus
+plot_variances(
+  plot_mean = plot_data$available_phosphorus,
+  plot_sd = plot_data$sd_available_phosphorus, x_axis = plot_data$elevation,
+  plot_name = "figures/available_phosphorus_vs_elevation.png",
+  x_unit = "Elevation (m)", y_unit = "Available Phosphorus (mg/kg)",
+  plot_title = "Variation in available phosphorus with elevation"
+)
+
+# Then look a variation vs EVI
+
+# For total carbon
+plot_variances(
+  plot_mean = plot_data$total_carbon, plot_sd = plot_data$sd_total_carbon,
+  x_axis = plot_data$evi, plot_name = "figures/total_carbon_vs_biomass.png",
+  plot_title = "Variation in total carbon with biomass",
+  x_unit = "Enhanced vegetation index", y_unit = "Total Carbon (%)"
+)
+
+# Total nitrogen
+plot_variances(
+  plot_mean = plot_data$total_nitrogen, plot_sd = plot_data$sd_total_nitrogen,
+  x_axis = plot_data$evi, plot_name = "figures/total_nitrogen_vs_biomass.png",
+  plot_title = "Variation in total nitrogen with biomass",
+  x_unit = "Enhanced vegetation index", y_unit = "Total Nitrogen (%)"
+)
+
+# Total phosphorus
+plot_variances(
+  plot_mean = plot_data$total_phosphorus, plot_sd = plot_data$sd_total_phosphorus,
+  x_axis = plot_data$evi, plot_name = "figures/total_phosphorus_vs_biomass.png",
+  plot_title = "Variation in total phosphorus with biomass",
+  x_unit = "Enhanced vegetation index", y_unit = "Total Phosphorus (mg/kg)"
+)
+
+# Available phosphorus
+plot_variances(
+  plot_mean = plot_data$available_phosphorus,
+  plot_sd = plot_data$sd_available_phosphorus, x_axis = plot_data$evi,
+  plot_name = "figures/available_phosphorus_vs_biomass.png",
+  x_unit = "Enhanced vegetation index", y_unit = "Available Phosphorus (mg/kg)",
+  plot_title = "Variation in available phosphorus with biomass"
+)
